62cd8f8c63404efd1d72c496c54711a2a5dc4902
HeroBuxx <me@herobuxx.me>
2023-10-27 02:51:42 +0700

    Add lat5
    Signed-off-by: HeroBuxx <me@herobuxx.me>

--- lat5.py
+++ lat5.py
@@ -1,9 +1,13 @@
 import socket
-from binascii import hexlify
 
-def int2byte@():
-    raw_data = 1234
-    # 32-Bit
-    print("Original: %s => Long Host Byte: => %s" )
+def convert2integer():
+    data = 1234
+    # 32-bit
+    print("original: %s ==> Long Host Byte Order: %s, Network byte order: %s"
+          % (data, socket.ntohl(data), socket.htonl(data)))
+    # 16-bit
+    print("original: %s ==> Short Host Byte Order: %s, Network byte order: %s"
+          % (data, socket.ntohs(data), socket.htons(data)))
+
 if __name__ == '__main__':
-    scan_service()
+    convert2integer()
